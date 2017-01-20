0e938357ed4f723fef548e843e26e7a7cefef5e2
Jayeson Kiyabu <jkiyabu@comcast.net>
2017-01-19 22:26:42 -0800

    add function to count up to user input value

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
+//business
+function countUp(input){
+
+  var newlist = [];
+  for (var list = 1; list <= input; list++) {
+      newlist.push(parseInt(list));
+  }
+  return newlist;
+
+}
+
+//user-interface
 $(document).ready(function() {
   $("form#pingPong").submit(function() {
     event.preventDefault();
     var input = $("#number").val();
-    console.log();
-    $("#result").text(input);
+    console.log(countUp(input));
+    $("#result").text(countUp(input));
     console.log();
   });
 });
